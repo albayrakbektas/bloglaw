ebd994ebfcafe87e6d7c4c9592cd865fb927eda0
Bektas Albayrak <albayrakbektas44@gmail.com>
2023-04-24 21:39:43 +0300

    Update bootstrap.js

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-// axios.defaults.baseURL = "https://bloglawserver-albayrakbektas.vercel.app/";
-axios.defaults.baseURL = "http://localhost:3000/";
+axios.defaults.baseURL = "https://bloglawserver-albayrakbektas.vercel.app/";
+// axios.defaults.baseURL = "http://localhost:3000/";
 
 window.axios = axios;
