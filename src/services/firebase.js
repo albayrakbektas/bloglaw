6091666227a3a7b03fdf1ef58ea974ab38d084d3
Bektas Albayrak <albayrakbektas44@gmail.com>
2023-04-24 19:15:27 +0300

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -5,18 +5,16 @@ import { getDatabase, ref as dbRef } from "firebase/database";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
-const firebaseConfig =
-  ({
-    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
-    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.VUE_APP_FIREBASE_APP_ID,
-    measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID,
-    databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
-  },
-  "blog-law");
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: "blog-law",
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+  measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
